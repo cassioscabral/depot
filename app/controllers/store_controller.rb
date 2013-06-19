1a0b343ef0bc6e1a45621777683151ab186b13ca
Cassio Cabral <cassioscabral@gmail.com>
2013-06-19 20:50:18 -0300

    atualizado até o capitulo adicionando um pouco de ajax, até a alteracao do do carrinho para aparecer na sidebar e colocado nas partials

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,11 +1,7 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
   def index
-    @products = Product.all
-
-    if session[:counter].nil?
-      session[:counter] = 1;
-    else
-      session[:counter] += 1
-    end
+  	@products = Product.order(:title)
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Depot::Application.config.session_store :encrypted_cookie_store, key: '_depot_session'
+Depot::Application.config.session_store :cookie_store
