749d3666c033fd43adf3a51c63fac9f290f239a2
Cassio Cabral <cassioscabral@gmail.com>
2013-06-19 21:23:57 -0300

    adicionado ajax no botao de adicionar ao carrinho

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,14 +26,15 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @cart = set_cart
+   # @cart = set_cart
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
-    session[:counter] = 0
+    #session[:counter] = 0
 
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
+        format.js
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
