68a4cf73f24b02e8f6e3ec4e9e4197d63ca2d5b7
Cassio Cabral <cassioscabral@gmail.com>
2013-06-21 22:32:19 -0300

    finishing section 12.1

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -5,6 +5,12 @@ class OrdersControllerTest < ActionController::TestCase
     @order = orders(:one)
   end
 
+  test "requires item in cart" do
+    get :new
+    assert_redirected_to store_path
+    assert_equal flash[:notice], 'Your cart is empty'
+  end
+
   test "should get index" do
     get :index
     assert_response :success
@@ -12,6 +18,12 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    item = LineItem.new
+    item.build_cart
+    item.product = products(:ruby)
+    item.save!
+    session[:cart_id] = item.cart.id
+    
     get :new
     assert_response :success
   end
@@ -21,7 +33,7 @@ class OrdersControllerTest < ActionController::TestCase
       post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     end
 
-    assert_redirected_to order_path(assigns(:order))
+    assert_redirected_to store_path
   end
 
   test "should show order" do
