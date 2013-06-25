e0f488b952dc438655deb7000fe7d581caf47ce9
Cassio Cabral <cassioscabral@gmail.com>
2013-06-25 15:26:32 -0300

    finishing section 14.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Depot::Application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  get "sessions/create"
+  get "sessions/destroy"
   resources :users
 
   resources :orders
