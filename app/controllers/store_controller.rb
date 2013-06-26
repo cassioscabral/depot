c01214bf3b7b6be1d7d68b0ee2bf5cc13baa4cf8
Cassio Cabral <cassioscabral@gmail.com>
2013-06-26 08:44:19 -0300

    test

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,6 +3,11 @@ class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
   def index
-  	@products = Product.order(:title)
+  	if params[:set_locale]
+  		redirect_to store_url(locale: params[:set_locale])
+  	else
+  		@products = Product.order(:title)
+  	end
+  	
   end
 end
