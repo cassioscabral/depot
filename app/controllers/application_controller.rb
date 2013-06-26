3746218f4a54bfeb45d190c1a7d35ee703bbce15
Cassio Cabral <cassioscabral@gmail.com>
2013-06-25 21:55:34 -0300

    finishing section 15.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,39 @@
 class ApplicationController < ActionController::Base
-	before_action :authorize
+  before_action :set_i18n_locale_from_params
+  # ...
+  before_action :authorize
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
-
   protected
 
-  	def authorize
-  		unless User.find_by(id: session[:user_id])
-  			redirect_to login_url, notice: "Please log in"
-  		end
-  	end
+    def authorize
+      if request.format == Mime::HTML 
+        unless User.find_by(id: session[:user_id])
+          redirect_to login_url, notice: "Please log in"
+        end
+      else
+        authenticate_or_request_with_http_basic do |username, password|
+          user = User.find_by(name: username)
+          user && user.authenticate(password)
+        end
+      end
+    end
+
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.include?(params[:locale].to_sym)
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] = 
+            "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
+
+    def default_url_options
+      { locale: I18n.locale }
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,71 +1,22 @@
 Depot::Application.routes.draw do
   get 'admin' => 'admin#index'
   controller :sessions do
-    get 'login' => :new
+    get  'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
-
+  get "sessions/create"
+  get "sessions/destroy"
 
   resources :users
-
-  resources :orders
-
-  resources :line_items
-
-  resources :carts
-
-  get "store/index"
   resources :products do
     get :who_bought, on: :member
   end
 
-  root :to => 'store#index', :as => 'store'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
 end
